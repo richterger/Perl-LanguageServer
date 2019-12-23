3e0fec1a069876a1a8da300cee4b97b38a5969df
Gerald Richter <richter@ecos.de>
2019-12-23 16:00:57 +0100

    Add retries to dbgforward in case dap client doesn't start fast enougth

--- clients/vscode/perl/out/dbgforward.js
+++ clients/vscode/perl/out/dbgforward.js
@@ -1,15 +1,36 @@
-var PORT = 13603;
+var port = 13603;
+var retries = 10 ;
+
+if (process.argv.length > 2)
+    port = parseInt(process.argv[2]) ;
 
 var net = require("net");
 
 var s = new net.Socket();
 
-s.on("data", function(data) {
-    process.stdout.write (data) ;
-});
-s.connect(PORT, '127.0.0.1', function(){
-    process.stdin.on('data', function (data) {
+s.on("error", function()
+    {
+    if (retries-- > 0)
+        {
+        setTimeout (function () 
+            {
+            s.connect(port, '127.0.0.1') ;
+            }, 200);
+        }
+    }) ;
+
+s.on("connect", function()
+    {
+    process.stdin.on('data', function (data) 
+        {
         s.write (data) ;
+        });
+    }) ;
+
+s.on("data", function(data) 
+    {
+    process.stdout.write (data) ;
     });
-});
+
+s.connect(port, '127.0.0.1') ;
 
