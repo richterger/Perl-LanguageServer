af895ae91ae825aef363125ad4b87ccabb4b020f
Gerald Richter <richter@ecos.de>
2021-07-11 15:05:18 +0200

    Fix: cwd paramter of launch.json had no effect

--- lib/Perl/LanguageServer/DebuggerProcess.pm
+++ lib/Perl/LanguageServer/DebuggerProcess.pm
@@ -129,13 +129,21 @@ sub lauch
         $ENV{$_} = $self -> env -> {$_} ;    
         }
 
+    my $cwd ;
+    if ($self -> cwd)
+        {
+        my $dir = $self -> cwd ;
+        $dir =~ s/'//g ;
+        $cwd = " chdir '$dir'; " ;
+        }
+
     my $inc = $workspace -> perlinc ;
     my @inc ;
     @inc = map { ('-I', $_)} @$inc if ($inc) ;
     
     $ENV{PLSDI_REMOTE} = '127.0.0.1:' . $self -> debug_adapter -> listen_port ;
     $ENV{PLSDI_OPTIONS} = $self -> reload_modules?'reload_modules':'' ;
-    $ENV{PERL5DB}      = 'BEGIN { $| = 1 ; require Perl::LanguageServer::DebuggerInterface }' ;
+    $ENV{PERL5DB}      = 'BEGIN { $| = 1 ; ' . $cwd . 'require Perl::LanguageServer::DebuggerInterface }' ;
     $ENV{PLSDI_SESSION}= $self -> session_id ;
     $pid = $self -> run_async ([$cmd, @inc, '-d', $fn, @{$self -> args}]) ;
     }
